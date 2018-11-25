7671c16ce481c4cb633ffc7845d58612cbeb61bb
Fabian Paul <fabian.paul@student.hpi.de>
2018-11-25 17:26:20 +0100

    Initial helper for median

---
+++ exercise_1/helper.sh
@@ -0,0 +1,28 @@
+run() 
+{
+    cmd=$1
+    end=$((SECONDS+2))
+    VALUES=(0)
+    while [ $SECONDS -lt $end ]; do
+        VALUES+=$( ($cmd)  )
+        sleep 1
+    done
+    echo $VALUES
+    median "${VALUES[@]}"
+}
+
+median() 
+{
+  arr=($(printf '%d\n' "${@}" | sort -n))
+  nel=${#arr[@]}
+  if (( $nel % 2 == 1 )); then     # Odd number of elements
+    val="${arr[ $(($nel/2)) ]}"
+  else                            # Even number of elements
+    (( j=nel/2 ))
+    (( k=j-1 ))
+    (( val=(${arr[j]} + ${arr[k]})/2 ))
+  fi
+  echo $val
+}
+
+run "echo 1"
