96ae4ff6403345c7bd5b62beb426734a3cd4010a
Fabian Paul <fabian.paul@student.hpi.de>
2018-11-26 23:07:11 +0100

    Use regex for transfer match on seq disk

--- exercise_1/scripts/measure-disk-sequential.sh
+++ exercise_1/scripts/measure-disk-sequential.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 source ./helper.sh
 
-
 measuresequential()
 {
-    # 
-    dd if=/dev/zero of=/tmp/bench bs=250M count=1 oflag=direct > disk-sequential-temp >&1
-    # Take last line of output file and then extract 10th argument (write/read speed)
-    result=$(cat disk-sequential-temp | tail -1 | cut -d " " -f 10)
+    result=$(dd if=/dev/zero of=~/testfile bs=250M count=1 oflag=direct |& grep -o -P '(?<=s,\s).*(?=[MG]B)')
     echo $result
 }
 run measuresequential "results-measure-disk-seq.csv"
