5082fa1549ea8bf443ba741769e0f610e5024004
martin <martin.fis@gmx.net>
2018-12-19 11:24:43 +0100

    adds error on task not exists

--- exercise_2/myct/myct.py
+++ exercise_2/myct/myct.py
@@ -7,9 +7,13 @@ import subprocess
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing argument: <task>")
-        exit()
+        exit(-1)
+
+    task = sys.argv[1]
+    if not task in ["ini", "map", "run"]:
+        print("Error: Task {} not supported.".format(task))
+        exit(-1) 
 
     pathname = os.path.dirname(os.path.realpath(__file__))
-    task = sys.argv[1]
     script = pathname+'/'+task+'.py'
     subprocess.call([sys.executable, script] + sys.argv[2:])
