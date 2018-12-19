76f93b5cf713d9130e8078d9054af95e02c0f0f8
martin <martin.fis@gmx.net>
2018-12-19 11:25:22 +0100

    adds arguments to executeable

--- exercise_2/myct/run.py
+++ exercise_2/myct/run.py
@@ -7,12 +7,14 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Runs the file executeable in container with arguments')
     parser.add_argument('container_path', type=str, help='Container')
+    parser.add_argument('--namespace', type=str, help='Namespace the executable runs in')
+    parser.add_argument('--limit', type=str, help='Limits the ressources for the executable')
     parser.add_argument('executeable', type=str, help='Executable')
-    args = parser.parse_args()
+    args, executeable_args = parser.parse_known_args()
 
     if not os.path.exists(args.container_path):
         print("Error: container path: {} does not exists.".format(args.container_path))
         exit(-1)
 
-    subprocess.call(["chroot", args.container_path, args.executeable])
+    subprocess.call(["chroot", args.container_path, args.executeable] + executeable_args)
     
