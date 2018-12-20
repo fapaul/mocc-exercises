a169372743f14bb91b3bcbc2caaafd3c76708412
martin <martin.fis@gmx.net>
2018-12-20 14:55:57 +0100

    fixes limit parameters

--- exercise_2/myct/run.py
+++ exercise_2/myct/run.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Runs the file executeable in container with arguments')
     parser.add_argument('container_path', type=str, help='Container')
     parser.add_argument('--namespace', type=str, help='Namespace the executable runs in')
-    parser.add_argument('--limit', type=str, help='Limits the ressources for the executable')
+    parser.add_argument('--limit', action='append', type=str, help='Limits the ressources for the executable')
     parser.add_argument('executeable', type=str, help='Executable')
     args, executeable_args = parser.parse_known_args()
 
@@ -19,17 +19,22 @@ if __name__ == '__main__':
     subprocess.call(["chroot", args.container_path, "mount", "-t", "proc", "proc", "/proc"])
 
     if args.limit:
-
-        limit_params =  args.limit.split("=")
-        controller = limit_params[0]
-        value = limit_params[1]
-
         cgroup_path = "/sys/fs/cgroup/memory/{}".format(args.container_path)
         
         if not os.path.exists(cgroup_path):
-            subprocess.call(["/bin/sh", "create_cgroup.sh", cgroup_path])
-        subprocess.call(["/bin/sh", "set_controller_value.sh", value, os.path.join(cgroup_path, controller)])
-        subprocess.call(["/bin/sh", "set_controller_value.sh", str(os.getppid()), os.path.join(cgroup_path, "tasks")])
+            os.makedirs(cgroup_path)
+
+        for limit in args.limit:
+            limit_params =  limit.split("=")
+            controller = limit_params[0]
+            value = limit_params[1]
+            f = open(os.path.join(cgroup_path, controller), "w")
+            f.write(value)
+            f.close()
+
+        f = open(os.path.join(cgroup_path, "tasks"), "a")
+        f.write(str(os.getpid()))
+        f.close()
 
     if not args.namespace:
         proc_mount_point = "{}/{}/proc".format(os.getcwd(), args.container_path)
